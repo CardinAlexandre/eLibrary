898a383906eb6c79e14d45bbb8bab89531147b02
Alexandre Cardin <alexandre.cardin@viveris.fr>
2025-10-08 14:18:42 +0200

    feat(auth): add AuthDbContext with Identity configuration

---
+++ services/auth-service/Data/AuthDbContext.cs
@@ -0,0 +1,25 @@
+using AuthService.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data;
+
+public class AuthDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<ApplicationUser>(entity =>
+        {
+            entity.Property(e => e.FirstName).HasMaxLength(100);
+            entity.Property(e => e.LastName).HasMaxLength(100);
+        });
+    }
+}
+
