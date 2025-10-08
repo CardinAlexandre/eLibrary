77c78085efcf60bd2b32ef5f7ce9cecc70b67b31
Alexandre Cardin <alexandre.cardin@viveris.fr>
2025-10-08 18:38:00 +0200

    feat(angular): add authentication state with NgRx effects

---
+++ frontend/angular/src/app/store/effects/auth.effects.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { map, catchError, switchMap, tap } from 'rxjs/operators';
+import { AuthService } from '../../services/auth.service';
+import * as AuthActions from '../actions/auth.actions';
+
+@Injectable()
+export class AuthEffects {
+  login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.login),
+      switchMap(({ email, password }) =>
+        this.authService.login(email, password).pipe(
+          map((response) => {
+            localStorage.setItem('token', response.token);
+            return AuthActions.loginSuccess({ user: response, token: response.token });
+          }),
+          catchError((error) =>
+            of(AuthActions.loginFailure({ error: error.message }))
+          )
+        )
+      )
+    )
+  );
+
+  loginSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.loginSuccess),
+        tap(() => this.router.navigate(['/']))
+      ),
+    { dispatch: false }
+  );
+
+  logout$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.logout),
+        tap(() => {
+          localStorage.removeItem('token');
+          this.router.navigate(['/login']);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+}
+
