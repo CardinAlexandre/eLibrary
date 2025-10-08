f28781744ae2c185078512c1660581eee8f4a45c
Alexandre Cardin <alexandre.cardin@viveris.fr>
2025-10-08 18:39:32 +0200

    feat(angular): configure app module with NgRx store and Material

---
+++ frontend/angular/src/app/app.module.ts
@@ -0,0 +1,79 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+// Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BookDetailsComponent } from './pages/book-details/book-details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+
+// Store
+import { booksReducer } from './store/reducers/books.reducer';
+import { authReducer } from './store/reducers/auth.reducer';
+import { themeReducer } from './store/reducers/theme.reducer';
+import { BooksEffects } from './store/effects/books.effects';
+import { AuthEffects } from './store/effects/auth.effects';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BookDetailsComponent,
+    LoginComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    
+    // Material
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    
+    // NgRx
+    StoreModule.forRoot({
+      books: booksReducer,
+      auth: authReducer,
+      theme: themeReducer
+    }),
+    EffectsModule.forRoot([BooksEffects, AuthEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25 })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
